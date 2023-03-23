6516db45908a568563e70549a75957b0b0e9dda1
Nikhil Nigam <nikhil.nigam@applitools.com>
2023-03-23 10:04:22 +0800

    nikhil

--- src/test/java/DemoApp/DemoApp.java
+++ src/test/java/DemoApp/DemoApp.java
@@ -27,8 +27,8 @@ public class DemoApp {
     private Eyes eyes;
     private static BatchInfo batch;
     private WebDriver driver;
-    private String url1 ="https://demo.applitools.com";
-    //private String url1 = "https://demo.applitools.com/index_v2.html";
+    //private String url1 ="https://demo.applitools.com";
+    private String url1 = "https://demo.applitools.com/index_v2.html";
     private String url2 ="https://demo.applitools.com/app.html";
     //private String url2 = "https://demo.applitools.com/app_v2.html";
 
